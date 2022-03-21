7eaba266d000d905dcc9f17014eb27b8ec644ff2
Jakub "Dukolm" Červený <dukolm@gmail.com>
2022-03-21 21:37:19 +0100

    build all version fix

--- pack.sh
+++ pack.sh
@@ -5,8 +5,8 @@ SELECT_SRD=$1
 version_env_name=$2
 DEPLOY_VERSION="${!version_env_name}"
 
-find ./builds/ -type f -exec sed -i -e "s/##SRD_DEPLOY_VERSION##/$DEPLOY_VERSION/g" {} \;
-find ./builds/ -type f -exec sed -i -e "s/##SRD_DEPLOY_DATE##/$SRD_DEPLOY_DATE/g" {} \;
+find ./builds/$SELECT_SRD -type f -exec sed -i -e "s/##SRD_DEPLOY_VERSION##/$DEPLOY_VERSION/g" {} \;
+find ./builds/$SELECT_SRD -type f -exec sed -i -e "s/##SRD_DEPLOY_DATE##/$SRD_DEPLOY_DATE/g" {} \;
 
 cd builds/$SELECT_SRD || { echo "cd Failure"; exit 1; }
 zip -r ../$SELECT_SRD-$DEPLOY_VERSION.zip public/*;
