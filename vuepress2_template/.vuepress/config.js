c5552c773225b7d77fcce920fdadd150371c5e61
Jakub "Dukolm" Červený <dukolm@gmail.com>
2024-04-02 20:34:43 +0200

    Update vuepress 2 to RC version

--- vuepress2_template/.vuepress/config.js
+++ vuepress2_template/.vuepress/config.js
@@ -1,42 +1,42 @@
 const srdConfig = require('./srd_config');
-const { path } = require('@vuepress/utils')
+import { path } from 'vuepress/utils'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 
-module.exports = {
-    // site config
+export default defineUserConfig({
     lang: 'cs',
     title: srdConfig.title,
     description: srdConfig.description,
 
+    plugins: [
+        googleAnalyticsPlugin({
+            id: srdConfig.googleAnalyticsId,
+        }),
+        searchPlugin({
+            maxSuggestions: 10,
+            locales: {
+                '/': {
+                    placeholder: 'Hledat',
+                }
+            },
+        }),
+        registerComponentsPlugin({
+            componentsDir: path.resolve(__dirname, './components'),
+        }),
+    ],
 
-    // theme and its config
-    theme: '@vuepress/theme-default',
-    themeConfig: {
+    bundler: viteBundler({
+        viteOptions: {},
+        vuePluginOptions: {
+
+        },
+    }),
+    theme: defaultTheme({
         navbar: srdConfig.navbar,
         sidebar: srdConfig.sidebar,
-    },
-    plugins: [
-        [
-            '@vuepress/plugin-search',
-            {
-                maxSuggestions: 10,
-                locales: {
-                    '/': {
-                        placeholder: 'Hledat',
-                    }
-                },
-            },
-        ],
-        [
-            '@vuepress/register-components',
-            {
-                componentsDir: path.resolve(__dirname, './components'),
-            },
-        ],
-        [
-            '@vuepress/plugin-google-analytics',
-            {
-                id: srdConfig.googleAnalyticsId,
-            },
-        ],
-    ],
-}
+    }),
+})
